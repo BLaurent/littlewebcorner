80762cd6cba8f11943f04457c441bd403383557d
Benoit Laurent <Benoit.Laurent@se.com>
2023-03-08 14:52:15 +0100

    Follow the tutorial + add edge-functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,38 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
 
 import netlify from "@astrojs/netlify/functions";
+
+const prettyCodeOptions = {
+  theme: "solarized-dark",
+  onVisitLine(node) {
+    if (node.children.length === 0) {
+      node.children = [
+        {
+          type: "text",
+          value: " ",
+        },
+      ];
+    }
+  },
+  onVisitHighlightedLine(node) {
+    node.properties.className.push("highlighted");
+  },
+  onVisitHighlightedWord(node) {
+    node.properties.className = ["word"];
+  },
+  tokensMap: {},
+};
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+  markdown: {
+    extendDefaultPlugins: true,
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+  },
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
