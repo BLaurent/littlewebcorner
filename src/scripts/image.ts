36cf3844230689098b128862c3d0dd3ffdfd646e
Benoit Laurent <Benoit.Laurent@se.com>
2023-04-06 18:35:18 +0200

    make canvas size more flexible

--- src/scripts/image.ts
+++ src/scripts/image.ts
@@ -5,14 +5,44 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xeff1f5);
 
 const canvas = document.getElementById("canvas-view") as HTMLCanvasElement;
+var heightRatio = 1;
+canvas.height = canvas.width * heightRatio;
+
+window.addEventListener("resize", resizeCanvas, false);
+function resizeCanvas() {
+  canvas.height = canvas.width * heightRatio;
+}
+
 const width = canvas.width;
 const height = canvas.height;
 
-const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-camera.position.z = 5;
+const target = new THREE.Vector3(0, 0, 0);
+const initialCameraPosition = new THREE.Vector3(
+  20 * Math.sin(0.2 * Math.PI),
+  10,
+  20 * Math.cos(0.2 * Math.PI)
+);
 
-const renderer = new THREE.WebGLRenderer({ canvas });
+const scale = height * 0.001 + 1.4;
+const camera = new THREE.OrthographicCamera(
+  -scale,
+  scale,
+  scale,
+  -scale,
+  0,
+  100
+);
+
+camera.position.copy(initialCameraPosition);
+camera.lookAt(target);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true,
+  antialias: true,
+});
 renderer.setSize(width, height);
+renderer.setClearColor(0x000000, 0);
 renderer.shadowMap.enabled = true;
 
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -25,15 +55,15 @@ const cube = new THREE.Mesh(geometry, material);
 cube.castShadow = true;
 scene.add(cube);
 
-const planeGeometry = new THREE.PlaneGeometry(10, 10);
-const planeMaterial = new THREE.MeshPhongMaterial({ color: 0xeff1f5 });
+const planeGeometry = new THREE.PlaneGeometry(200, 200);
+const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xeff1f5 });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.receiveShadow = true;
-plane.rotation.x = -Math.PI / 2;
+plane.rotation.x = Math.PI / 2;
 plane.position.y = -0.5;
 scene.add(plane);
 
-const directional = new THREE.DirectionalLight(0xeff1f5, 0.5);
+const directional = new THREE.DirectionalLight(0xeff1f5, 1);
 directional.position.set(10, 10, 10);
 directional.castShadow = true;
 directional.shadow.camera.near = 0.1;
@@ -46,16 +76,15 @@ directional.shadow.mapSize.width = 256;
 directional.shadow.mapSize.height = 256;
 scene.add(directional);
 
-const ambient = new THREE.AmbientLight(0xeff1f5, 0.5);
-ambient.castShadow = true;
+const ambient = new THREE.AmbientLight(0xeff1f5, 0.01);
 scene.add(ambient);
 
 scene.add(camera);
 
 function animate() {
   requestAnimationFrame(animate);
-  // cube.rotation.x += 0.01;
-  // cube.rotation.y += 0.01;
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
   controls.update();
   renderer.render(scene, camera);
 }
